de82c2eeea39d5f382304b88fca7e06168909af8
Moritz Pereira <mpereirakr@stud.hs-heilbronn.de>
2022-10-04 16:04:57 +0200

    Service Interface updated.

--- apis/src/main/java/de/hhn/it/devtools/apis/pwmanager/PwManagerService.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/pwmanager/PwManagerService.java
@@ -7,16 +7,39 @@ public interface PwManagerService {
 
     /**
      * changes the Master Password.
+     *
      * @param password the new master password.
      */
-    private void changeMasterPw(String password){
+    private void changeMasterPw(String password) {
 
     }
 
     /**
      * changes the visibility of the passwords.
      */
-    private void changeHidden(){
+    private void changeHidden() {
+
+    }
+
+    /**
+     * Creates a new Entry.
+     *
+     * @param url      for the associated password.
+     * @param username for the associated password.
+     * @param email    for the associated password.
+     * @param password
+     * @return the created entry.
+     */
+    private Entry addEntry(String url, String username, String email, String password) {
+        Entry entry = new Entry(url, username, email, password);
+        return entry;
+    }
+
+    /**
+     * Deletes an entry.
+     * @param entry that will be deleted.
+     */
+    private void deleteEntry(Entry entry){
 
     }
 }
