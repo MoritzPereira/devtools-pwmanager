eb259fc34d93470405c7536e47b2eff7efb3ad60
Sven Hasse <shasse@stud.hs-heilbronn.de>
2022-10-04 10:57:17 +0200

    added constructor and other useful methods

--- apis/src/main/java/de/hhn/it/devtools/apis/pwmanager/Entry.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/pwmanager/Entry.java
@@ -1,4 +1,36 @@
 package de.hhn.it.devtools.apis.pwmanager;
 
 public class Entry {
+
+    private String url;
+    private String username;
+    private String email;
+    private String password;
+
+    public Entry(String url, String username, String email, String password){
+
+        this.url = url;
+        this.username = username;
+        this.email = email;
+        this.password = hash(password);
+        
+    }
+
+    private void changeUrl(String url){
+
+    }
+    private void changeUsername(String username){
+
+    }
+    private void changeEmail(String email){
+
+    }
+    private void changePassword(String password){
+
+    }
+
+    private String hash(String password){
+        return "HASHEDPW";
+    }
+
 }
