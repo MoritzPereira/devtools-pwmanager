3908c04b7e2498b735ba3951c2fcb8f72feb7b2b
Florin Schreier <florin.pierre30@gmail.com>
2022-10-04 11:11:46 +0200

    pwmanagerListener improved

--- apis/src/main/java/de/hhn/it/devtools/apis/pwmanager/pwmanagerListener.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/pwmanager/pwmanagerListener.java
@@ -1,4 +1,18 @@
 package de.hhn.it.devtools.apis.pwmanager;
 
 public interface pwmanagerListener {
+
+    void loggedin();
+
+    void loggedout();
+
+    void Entryadded(Entry newEntry);
+
+    void Entrydeleted(Entry currentEntry);
+
+    void Entrychanged();
+
+    void changepasswordvisibility();
+
+    void updateList();
 }
