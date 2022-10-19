a6a7ff7dc08d9f78ff8f07cee296ab339c5e1cb6
agres <agres.dominik@gmail.com>
2022-10-19 20:28:25 +0200

    ToDo class reworked, entry interface reworked

--- apis/src/main/java/de/hhn/it/devtools/apis/todolist/ToDoEntry.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/todolist/ToDoEntry.java
@@ -6,33 +6,14 @@ public interface ToDoEntry {
      * creates a new todo in the todo list
      * @param title title of the new todo
      * @param description description of the new todo
+     * @param checked checked status of the todo
      */
-    ToDo createTodo(String title, String description);
+    ToDo createTodo(String title, String description, boolean checked);
 
     /**
      * deletes a todo of the todo-list
      */
     void deleteTodo();
 
-    /**
-     * edits a title of a todo
-     * @param newTitle the new title of the todo
-     */
-    void editTitle(String newTitle);
 
-    /**
-     * edits a description of a todo
-     * @param newDescription the new description of the todo
-     */
-    void editDescription(String newDescription);
-
-    /**
-     * marks a todo as "done"
-     */
-    void checkTodo();
-
-    /**
-     * marks a done todo as "undone"
-     */
-    void uncheckTodo();
 }
