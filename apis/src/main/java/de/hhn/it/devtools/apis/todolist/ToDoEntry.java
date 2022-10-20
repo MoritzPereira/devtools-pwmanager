52af7de75b1df0502e4c1b0828c611c55dfdd84e
Dennis <dennisdeifel@gmail.com>
2022-10-20 10:58:28 +0200

    nur test commit

--- apis/src/main/java/de/hhn/it/devtools/apis/todolist/ToDoEntry.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/todolist/ToDoEntry.java
@@ -1,12 +1,11 @@
 package de.hhn.it.devtools.apis.todolist;
 
-/**
- * Interface for managing todos in the Task-List.
- */
+/** Interface for managing todos in the Task-List. */
 public interface ToDoEntry {
 
     /**
      * creates a new todo in the todo list.
+     *
      * @param title title of the new todo
      * @param description description of the new todo
      * @param checked checked status of the todo
@@ -15,16 +14,16 @@ public interface ToDoEntry {
 
     /**
      * deletes a todo of the todo-list.
+     *
      * @param todoId, the ID of the task to delete
      */
     void deleteTodo(int todoId);
 
     /**
      * edits a todo of the todo-list.
+     *
      * @param title, edits the title of the task
      * @param description, edits the description of the task
      */
     void editTodo(String title, String description);
-
-
 }
