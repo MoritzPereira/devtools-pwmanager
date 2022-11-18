f8ad49c52d7ebf6f4be24cb3f7db0fa9476943b4
fschoenwae <fschoenwae@stud.hs-heilbronn.de>
2022-11-18 23:30:00 +0100

    Reverted changes for Bill and BillEntry

--- apis/src/main/java/de/hhn/it/devtools/apis/supermarketsystem/Bill.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/supermarketsystem/Bill.java
@@ -1,7 +1,5 @@
 package de.hhn.it.devtools.apis.supermarketsystem;
 
-import de.hhn.it.devtools.apis.supermarketsystem.BillEntry;
-import de.hhn.it.devtools.apis.supermarketsystem.Product;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,48 +7,86 @@ import java.util.stream.Collectors;
 /** Represents the bill.
  *
  */
-public interface Bill {
+public class Bill {
+  private final HashMap<Integer, BillEntry> productList;
 
-  public Map<Integer, BillEntry> getList();
+  private float summary;
 
-  public float getSummary();
+  public Bill() {
+    productList = new HashMap<>();
+    summary = 0F;
+  }
+
+  public Map<Integer, BillEntry> getList() {
+    return productList;
+  }
+
+  public float getSummary() {
+    return summary;
+  }
 
   /** Returns the product list of the bill.
    *
    * @return product list
    */
-  public Map<Integer, Product> getProducts();
+  public Map<Integer, Product> getProducts() {
+    return ((HashMap<Integer, BillEntry>) productList.clone())
+        .entrySet()
+        .stream()
+        .filter(entry -> entry.getKey() >= 0)
+        .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().getProduct()));
+  }
 
   /** Adds a bill entry to the bill.
    *
    * @param id id of the product
    * @param billEntry bill entry
    */
-  public void addBillEntry(int id, BillEntry billEntry);
+  public void addBillEntry(int id, BillEntry billEntry) {
+    productList.put(id, billEntry);
+  }
 
   /** Removes a bill entry by the product id.
    *
    * @param id id of the product
    */
-  public void removeEntry(int id);
+  public void removeEntry(int id) {
+    productList.remove(id);
+  }
 
   /** Checks if the product list contains the given key.
    *
    * @param id id of a product
    * @return true/false
    */
-  public boolean containsKey(int id);
+  public boolean containsKey(int id) {
+    return productList.containsKey(id);
+  }
 
   /** Returns a entry from the product list.
    *
    * @param id id of the product
    * @return bill entry
    */
-  public BillEntry getEntry(int id);
+  public BillEntry getEntry(int id) {
+    return productList.get(id);
+  }
 
   /** Recalculates the bill summary.
    *
    */
-  public void recalculate();
+  public void recalculate() {
+    summary = 0F;
 
+    for (BillEntry entry : productList.values()) {
+      summary += entry.getPrice() * entry.getQuantity();
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "Bill:\n"
+        + "Products:\n" + productList
+        + "\nSummary\n" + summary;
+  }
 }
--- apis/src/main/java/de/hhn/it/devtools/apis/supermarketsystem/BillEntry.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/supermarketsystem/BillEntry.java
@@ -1,51 +1,97 @@
 package de.hhn.it.devtools.apis.supermarketsystem;
 
-
 /** Represents a single entry of the bill.
  *
  */
-public interface BillEntry {
+public class BillEntry {
 
+  private final float price;
+  private int quantity;
 
-    /** Returns the product from the BillEntry.
-     *
-     * @return product
-     */
-    Product getProduct();
+  private final Product product;
 
-    /** Returns the calculated price of the entry.
-     *
-     * @return price
-     */
-    float getPrice();
+  /** Creates a new instance of a bill entry.
+   *
+   * @param product linked product
+   */
+  public BillEntry(Product product) {
+    this.product = product;
+    this.price = product.getPrice();
+    this.quantity = 1;
+  }
 
-    /** Returns the quantity of the entry.
-     *
-     * @return quantity
-     */
-    int getQuantity();
+  /** Returns the product from the BillEntry.
+   *
+   * @return product
+   */
+  public Product getProduct() {
+    return product;
+  }
 
-    /** Raises the quantity by once.
-     *
-     */
-    void addQuantity();
+  /** Returns the calculated price of the entry.
+   *
+   * @return price
+   */
+  public float getPrice() {
+    return price;
+  }
 
-    /** Raises the quantity by the given amount.
-     *
-     * @param quantity quantity amount
-     */
-    void addQuantity(int quantity);
+  /** Returns the quantity of the entry.
+   *
+   * @return quantity
+   */
+  public int getQuantity() {
+    return quantity;
+  }
 
-    /** Reduces the quantity by once.
-     *
-     */
-    void reduceQuantity();
+  /** Raises the quantity by once.
+   *
+   */
+  public void addQuantity() {
+    this.quantity++;
+  }
 
-    /** Reduces the quantity by the given amount.
-     *
-     * @param quantity quantity amount
-     */
-    void reduceQuantity(int quantity);
+  /** Raises the quantity by the given amount.
+   *
+   * @param quantity quantity amount
+   */
+  public void addQuantity(int quantity) {
+    this.quantity += quantity;
+  }
 
+  /** Reduces the quantity by once.
+   *
+   */
+  public void reduceQuantity() {
+
+    if (this.quantity < 0) {
+      throw new IllegalStateException("BillEntry quantity is lower then 0");
+    }
+
+    this.quantity--;
+  }
+
+  /** Reduces the quantity by the given amount.
+   *
+   * @param quantity quantity amount
+   */
+  public void reduceQuantity(int quantity) {
+
+    if (this.quantity - quantity < 0) {
+      throw new IllegalStateException("BillEntry quantity is lower then 0");
+    }
+
+    this.quantity -= quantity;
+  }
+
+  @Override
+  public String toString() {
+
+    return String.format(
+        "\nBillEntry:\nName: %s\nPrice: %.2f\nQuantity: %d\n",
+        product == null ? "Summe" : product.getName(),
+        price,
+        quantity
+    );
+  }
 }
-
