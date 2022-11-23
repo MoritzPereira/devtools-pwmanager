18aecf0961349a124bc90b0a63ef958684c80e71
kliebisch <kliebisch@stud.hs-heilbronn.de>
2022-11-23 20:26:22 +0100

    UsageDemo Finished 23.11.2022

--- components/src/test/java/de/hhn/it/devtools/components/supermarketsystem/SupermarketServiceUsageDemo.java
+++ components/src/test/java/de/hhn/it/devtools/components/supermarketsystem/SupermarketServiceUsageDemo.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import de.hhn.it.devtools.apis.exceptions.IllegalParameterException;
+import de.hhn.it.devtools.apis.supermarketsystem.BillEntry;
 import de.hhn.it.devtools.apis.supermarketsystem.Product;
 import de.hhn.it.devtools.apis.supermarketsystem.ProductCategory;
 import java.io.IOException;
@@ -15,9 +16,12 @@ import java.nio.file.Paths;
 import java.util.Map;
 
 public class SupermarketServiceUsageDemo {
+
+  static SupermarketServerService supermarketServerService;
+
   public static void main(String[] args) throws IllegalParameterException, IllegalStateException {
     // sets up the SupermarketServerService.
-    SupermarketServerService supermarketServerService = new SupermarketServerService();
+    addProductsToSystem();
 
     /*
     -> Produkte werden zum Kassenzettel hinzugefügt
@@ -37,13 +41,17 @@ public class SupermarketServiceUsageDemo {
     supermarketServerService.addProductToBill(3);
 
     //all Products on the bill
-    Map<Integer, Product> scannedProducts = supermarketServerService.getProductsFromBill();
+    Map<Integer, BillEntry> scannedProducts = supermarketServerService.getBill().getList();
+
+    System.out.println();
 
     System.out.println("Scanned products: ");
-    for(int i = 0; i < scannedProducts.size(); i++) {
-      System.out.println(scannedProducts.get(i));
+    for(BillEntry billEntry : scannedProducts.values()) {
+      System.out.println(billEntry);
       System.out.println("---------");
     }
+
+    System.out.println();
 
     // deletes a product from the bill.
     supermarketServerService.deleteProductFromBill(3);
@@ -52,25 +60,32 @@ public class SupermarketServiceUsageDemo {
     supermarketServerService.getProductsFromBill();
 
     System.out.println("Scanned products after delete");
-    for(int i = 0; i < scannedProducts.size(); i++){
-      System.out.println(scannedProducts.get(i));
+    for(BillEntry billEntry : scannedProducts.values()) {
+      System.out.println(billEntry);
       System.out.println("---------");
     }
+
+    System.out.println();
 
     // shows a list of all available products.
     Map<Integer, Product> availableProducts = supermarketServerService.getProducts();
 
     System.out.println("Available products at supermarket");
-    for(int i= 0; i < availableProducts.size(); i++){
-      System.out.println(availableProducts.get(i));
-      System.out.println("--------");
+    for(Product product : availableProducts.values()) {
+      System.out.println(product);
+      System.out.println("---------");
     }
 
+    System.out.println();
+
     // Shows information about a specific product.
+    System.out.println("Display product with id 6");
     System.out.println(supermarketServerService.getProduct(6));
 
     // adds that Product to bill
     supermarketServerService.addProductToBill(6);
+
+    System.out.println();
 
     // shows the total price of the bill.
     System.out.println("Total price: ");
@@ -81,5 +96,41 @@ public class SupermarketServiceUsageDemo {
     System.out.println(supermarketServerService.calculateChange(50.0F));
 
   }
+  public static void addProductsToSystem() {
+    supermarketServerService = new SupermarketServerService();
+
+    System.out.println("Prepare Products");
+    try {
+      // Read File from System
+      Path filePath = Paths.get("./components/src/test/java/de/hhn/it/devtools/components/supermarketsystem/Products.json");
+      Reader reader = Files.newBufferedReader(filePath);
+      JsonArray parser = JsonParser.parseReader(reader).getAsJsonArray();
+
+      // Loop through entries
+      for(JsonElement jsonElement : parser) {
+        // Convert entry to json object
+        JsonObject productData = jsonElement.getAsJsonObject();
+
+        /* Get Values from entry */
+        int id      = productData.get("id").getAsInt();
+        String name = productData.get("name").getAsString();
+        float price = productData.get("price").getAsFloat();
+        String manufacturer = productData.get("manufacturer").getAsString();
+        String category     = productData.get("category").getAsString();
+        ProductCategory productCategory = ProductCategory.valueOf(category.toUpperCase());
+
+        // Add product to service
+        supermarketServerService.addProduct(id, name, price, manufacturer, productCategory);
+
+        System.out.println("Adding product: " + name);
+      }
+
+      System.out.println("All products added to the store");
+    } catch (IOException ioException) {
+      System.out.println("File not found");
+      System.out.println(ioException.getMessage());
+    }
+  }
 }
+
 
