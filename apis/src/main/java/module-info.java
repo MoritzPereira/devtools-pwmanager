86dfa7f5b7addd17859bb2eb436782ae687e32c6
MoritzPereira <momoperkru@gmail.com>
2022-11-24 03:51:44 +0100

    .

--- apis/src/main/java/module-info.java
+++ apis/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module devtools.apis{
         requires org.slf4j;
         exports de.hhn.it.devtools.apis.examples.coffeemakerservice;
         exports de.hhn.it.devtools.apis.exceptions;
+        exports de.hhn.it.devtools.apis.supermarketsystem;
         exports de.hhn.it.devtools.apis.pwmanager.exceptions;
         exports de.hhn.it.devtools.apis.pwmanager;
 }
