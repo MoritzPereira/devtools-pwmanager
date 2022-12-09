dd453eab9ea00982c7978a55440fe07c1ce272c0
SvenHasse <svenh60@web.de>
2022-12-09 15:11:35 +0100

    listenerTest added

---
+++ components/src/test/java/de/hhn/it/devtools/components/pwmanager/junit/DummyListener.java
@@ -0,0 +1,73 @@
+package de.hhn.it.devtools.components.pwmanager.junit;
+
+import de.hhn.it.devtools.apis.pwmanager.Entry;
+import de.hhn.it.devtools.apis.pwmanager.PwManagerListener;
+import de.hhn.it.devtools.components.example.coffeemakerservice.junit.DummyCallback;
+
+import java.util.ArrayList;
+
+public class DummyListener implements PwManagerListener {
+
+    private static final org.slf4j.Logger logger =
+            org.slf4j.LoggerFactory.getLogger(DummyCallback.class);
+
+    @Override
+    public void loggingin(String masterPw) {
+        logger.info("Logging in...");
+    }
+
+    @Override
+    public void loggedin() {
+        logger.info("Logged in");
+    }
+
+    @Override
+    public void logout() {
+        logger.info("Logging out...");
+    }
+
+    @Override
+    public void loggedout() {
+        logger.info("Loggt out...");
+    }
+
+    @Override
+    public void entryAdded(Entry newEntry) {
+        logger.info("Entry added");
+    }
+
+    @Override
+    public void entryDeleted(Entry currentEntry) {
+        logger.info("Entry deleted");
+    }
+
+    @Override
+    public void entryChanged(Entry entry) {
+        logger.info("Entry changed");
+    }
+
+    @Override
+    public void changePasswordVisibility(int id) {
+        logger.info("Passsword-Visibility changed from id: " + id);
+    }
+
+    @Override
+    public void updateEntryList() {
+        logger.info("Entrylist updated");
+    }
+
+    @Override
+    public void generatePw(boolean useUpper, boolean useLower, boolean useDigits, boolean useSpecialChars) {
+        logger.info("New password generated");
+    }
+
+    @Override
+    public void showNewPw(String pw) {
+        logger.info("Showing new password");
+    }
+
+    @Override
+    public void showsortedEntryList(ArrayList<Entry> entryList) {
+        logger.info("Showing sorted entry list");
+    }
+}
--- components/src/test/java/de/hhn/it/devtools/components/pwmanager/junit/TestPwManagerService.java
+++ components/src/test/java/de/hhn/it/devtools/components/pwmanager/junit/TestPwManagerService.java
@@ -1,7 +1,9 @@
 package de.hhn.it.devtools.components.pwmanager.junit;
 
+import de.hhn.it.devtools.apis.exceptions.IllegalParameterException;
 import de.hhn.it.devtools.apis.pwmanager.PwManagerListener;
 import de.hhn.it.devtools.apis.pwmanager.PwManagerService;
+import de.hhn.it.devtools.apis.pwmanager.exceptions.IllegalMasterPasswordException;
 import de.hhn.it.devtools.components.pwmanager.provider.SimplePwManagerService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,11 +15,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestPwManagerService {
 
   private SimplePwManagerService pwManagerService;
+  private DummyListener pwManagerListener;
+  private String masterPw;
 
 
   @BeforeEach
   void setup() {
+
     pwManagerService = new SimplePwManagerService();
+    masterPw = "admin";
+    pwManagerListener = new DummyListener();
+
   }
 
 
@@ -27,15 +35,31 @@ public class TestPwManagerService {
    * Tests if listeners get added correctly.
    */
   void testAddListener() {
-    PwManagerListener listener = null;
 
     //Good Case
 
 
     //Bad Case
     Assertions.assertThrows(NullPointerException.class, ()->{
-      pwManagerService.addListener(listener);
+      pwManagerService.addListener(pwManagerListener);
     });
 
   }
+
+  @Test
+  void testChangeMasterPw(){
+
+    Assertions.assertThrows(IllegalMasterPasswordException.class, ()->{
+      pwManagerService.changeMasterPw("secret", "admin");
+    });
+
+    Assertions.assertThrows(IllegalParameterException.class, ()->{
+      pwManagerService.changeMasterPw("secret", masterPw);
+      masterPw = "secret";
+    });
+
+  }
+
+
+
 }
