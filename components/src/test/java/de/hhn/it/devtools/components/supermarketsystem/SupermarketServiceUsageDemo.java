af3a3eab9f99cd043727f3e4a91ddeb6499ffef1
fschoenwae <fschoenwae@stud.hs-heilbronn.de>
2022-12-16 15:01:52 +0100

    Commit again cause of file was overwritten

--- components/src/test/java/de/hhn/it/devtools/components/supermarketsystem/SupermarketServiceUsageDemo.java
+++ components/src/test/java/de/hhn/it/devtools/components/supermarketsystem/SupermarketServiceUsageDemo.java
@@ -1,20 +1,10 @@
 package de.hhn.it.devtools.components.supermarketsystem;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import de.hhn.it.devtools.apis.exceptions.IllegalParameterException;
 import de.hhn.it.devtools.apis.supermarketsystem.BillEntry;
 import de.hhn.it.devtools.apis.supermarketsystem.Product;
-import de.hhn.it.devtools.apis.supermarketsystem.ProductCategory;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SupermarketServiceUsageDemo {
@@ -25,7 +15,10 @@ public class SupermarketServiceUsageDemo {
 
   public static void main(String[] args) throws IllegalParameterException, IllegalStateException {
     // sets up the SupermarketServerService.
-    addProductsToSystem();
+    supermarketServerService = new SupermarketServerService();
+
+    List<Product> productList = ProductGenerator.getSampleProductList(true);
+    supermarketServerService.addProducts(productList);
 
     /*
     -> Produkte werden zum Kassenzettel hinzugefügt
@@ -47,11 +40,15 @@ public class SupermarketServiceUsageDemo {
     //all Products on the bill
     Map<Integer, BillEntry> scannedProducts = supermarketServerService.getBill().getList();
 
-    logger.info("Scanned Products" + scannedProducts);
+    System.out.println();
+
+    System.out.println("Scanned products: ");
     for(BillEntry billEntry : scannedProducts.values()) {
-      logger.info("" + billEntry + "\n");
-      logger.info("---------\n");
+      System.out.println(billEntry);
+      System.out.println("---------");
     }
+
+    System.out.println();
 
     // deletes a product from the bill.
     supermarketServerService.deleteProductFromBill(3);
@@ -59,71 +56,42 @@ public class SupermarketServiceUsageDemo {
     // shows a list of all scanned products after deletion
     supermarketServerService.getProductsFromBill();
 
-    logger.info("Scanned products after delete" + scannedProducts);
+    System.out.println("Scanned products after delete");
     for(BillEntry billEntry : scannedProducts.values()) {
-      logger.info("" + billEntry + "\n");
-      logger.info("---------\n");
+      System.out.println(billEntry);
+      System.out.println("---------");
     }
 
+    System.out.println();
 
     // shows a list of all available products.
     Map<Integer, Product> availableProducts = supermarketServerService.getProducts();
 
-    logger.info("Available products at supermarket");
+    System.out.println("Available products at supermarket");
     for(Product product : availableProducts.values()) {
-      logger.info("" + product + "\n");
-      logger.info("---------\n");
+      System.out.println(product);
+      System.out.println("---------");
     }
 
+    System.out.println();
+
     // Shows information about a specific product.
-    logger.info("Display product with id: 6 \n" + supermarketServerService.getProduct(6) + "\n");
+    System.out.println("Display product with id 6");
+    System.out.println(supermarketServerService.getProduct(6));
 
     // adds that Product to bill
     supermarketServerService.addProductToBill(6);
 
+    System.out.println();
 
     // shows the total price of the bill.
-    float price = supermarketServerService.getBill().getSummary();
-    logger.info("Total price: " + price + "\n");
+    System.out.println("Total price: ");
+    System.out.println(supermarketServerService.getBill().getSummary());
 
     // shows the change the customer gets.
-    float change = supermarketServerService.calculateChange(50.0F);
-    System.out.println("Change for the customer: " + change + "\n");
+    System.out.println("Change for the customer: ");
+    System.out.println(supermarketServerService.calculateChange(50.0F));
 
-  }
-  public static void addProductsToSystem() {
-    supermarketServerService = new SupermarketServerService();
-
-    logger.info("Prepare Products\n");
-    try {
-      // Read File from System
-      Path filePath = Paths.get("./components/src/test/java/de/hhn/it/devtools/components/supermarketsystem/Products.json");
-      Reader reader = Files.newBufferedReader(filePath);
-      JsonArray parser = JsonParser.parseReader(reader).getAsJsonArray();
-
-      // Loop through entries
-      for(JsonElement jsonElement : parser) {
-        // Convert entry to json object
-        JsonObject productData = jsonElement.getAsJsonObject();
-
-        /* Get Values from entry */
-        int id      = productData.get("id").getAsInt();
-        String name = productData.get("name").getAsString();
-        float price = productData.get("price").getAsFloat();
-        String manufacturer = productData.get("manufacturer").getAsString();
-        String category     = productData.get("category").getAsString();
-        ProductCategory productCategory = ProductCategory.valueOf(category.toUpperCase());
-
-        // Add product to service
-        supermarketServerService.addProduct(id, name, price, manufacturer, productCategory);
-        logger.info("Adding product: " + name + "\n");
-      }
-
-      logger.info("All products added to the store.\n");
-    } catch (IOException ioException) {
-      logger.warn("File not found!\n");
-      logger.warn("" + ioException.getMessage());
-    }
   }
 }
 
