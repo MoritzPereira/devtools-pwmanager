acdf5309c79c89d0aec8e892adc04c076fb59e72
MoritzPereira <momoperkru@gmail.com>
2023-02-21 09:44:13 +0100

    Javadoc added

--- apis/src/main/java/de/hhn/it/devtools/apis/pwmanager/exceptions/IllegalMasterPasswordException.java
+++ apis/src/main/java/de/hhn/it/devtools/apis/pwmanager/exceptions/IllegalMasterPasswordException.java
@@ -8,6 +8,9 @@ public class IllegalMasterPasswordException extends Exception {
   private static final org.slf4j.Logger logger =
           org.slf4j.LoggerFactory.getLogger(IllegalMasterPasswordException.class);
 
+  /**
+   * Exception if masterpw is wrong.
+   */
   public IllegalMasterPasswordException() {
     super("Wrong master password!");
   }
